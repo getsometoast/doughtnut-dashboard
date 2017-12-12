664b6b964c8565feda05d7b6ea84cc776e5298e1
James Lewis <jamesl@thoughtworks.com>
2017-12-12 12:16:13 +1100

    refactor Home container to be a react class

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,11 +1,10 @@
-import React from 'react'
+import React, { Component } from 'react';
 import { push } from 'react-router-redux'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import RaisedButton from 'material-ui/RaisedButton'
 import FlatButton from 'material-ui/FlatButton'
 import { Grid, Row, Col } from 'react-flexbox-grid';
-
 import {
   increment,
   incrementAsync,
@@ -13,70 +12,74 @@ import {
   decrementAsync
 } from '../../modules/counter'
 
-const Home = props => (
-  <Grid fluid style={{paddingTop: '1rem'}}>
-    <Row style={{marginBottom: '1rem'}}>
-      <Col xs={6} md={6}>
-        <RaisedButton 
-          label='Increment'
-          onClick={props.increment}
-          disabled={props.isIncrementing}
-          primary={true}
-          style={{
-            width: '100%',
-          }}
-        />
-      </Col>
-      <Col xs={6} md={6}>
-        <RaisedButton 
-          label='Increment Async'
-          onClick={props.incrementAsync} 
-          disabled={props.isIncrementing}
-          primary={true}
-          style={{
-            width: '100%',
-          }}
-        />
-      </Col>
-    </Row>
-    <Row style={{marginBottom: '1rem'}}>
-      <Col xs={6} md={6}>
-        <RaisedButton 
-          label='Decrement'
-          onClick={props.decrement}
-          disabled={props.isDecrementing}
-          secondary={true}
-          style={{
-            width: '100%',
-          }}
-        />
-      </Col>
-      <Col xs={6} md={6}>
-        <RaisedButton 
-          label='Decrement Async'
-          onClick={props.decrementAsync} 
-          disabled={props.isDecrementing} 
-          secondary={true}
-          style={{
-            width: '100%',
-          }}
-        />
-      </Col>
-    </Row>
-    <Row>
-      <Col xs={12} md={12}>  
-        <FlatButton
-          label='Go to about page via redux'
-          onClick={() => props.changePage()}
-          primary={true}
-          style={{
-            width: '100%'
-          }}
-        />
-      </Col>
-    </Row>
-  </Grid>
-)
+class Home extends Component {
+  render() {
+    return (
+      <Grid fluid style={{paddingTop: '1rem'}}>
+        <Row style={{marginBottom: '1rem'}}>
+          <Col xs={6} md={6}>
+            <RaisedButton 
+              label='Increment'
+              onClick={this.props.increment}
+              disabled={this.props.isIncrementing}
+              primary={true}
+              style={{
+                width: '100%',
+              }}
+            />
+          </Col>
+          <Col xs={6} md={6}>
+            <RaisedButton 
+              label='Increment Async'
+              onClick={this.props.incrementAsync} 
+              disabled={this.props.isIncrementing}
+              primary={true}
+              style={{
+                width: '100%',
+              }}
+            />
+          </Col>
+        </Row>
+        <Row style={{marginBottom: '1rem'}}>
+          <Col xs={6} md={6}>
+            <RaisedButton 
+              label='Decrement'
+              onClick={this.props.decrement}
+              disabled={this.props.isDecrementing}
+              secondary={true}
+              style={{
+                width: '100%',
+              }}
+            />
+          </Col>
+          <Col xs={6} md={6}>
+            <RaisedButton 
+              label='Decrement Async'
+              onClick={this.props.decrementAsync} 
+              disabled={this.props.isDecrementing} 
+              secondary={true}
+              style={{
+                width: '100%',
+              }}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} md={12}>  
+            <FlatButton
+              label='Go to about page via redux'
+              onClick={() => this.props.changePage()}
+              primary={true}
+              style={{
+                width: '100%'
+              }}
+            />
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   count: state.counter.count,
