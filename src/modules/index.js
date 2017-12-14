49ba1afae260cb1709565614d6e0d2267e89b9f1
James Lewis <jamesl@thoughtworks.com>
2017-12-14 15:28:31 +1100

    fix error in the root reducer

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 import count from '../components/buttons/countButton/reducers'
-import layout from '../layout/reducers'
+import layout from '../pages/layout/reducers'
 
 export default combineReducers({
   routing: routerReducer,
