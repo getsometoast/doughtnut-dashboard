6485314fbb88a97c27a3e079d40916a4381efa56
James Lewis <hijameslewis@gmail.com>
2018-02-09 13:38:43 -0500

    map out the master layout

--- src/sandbox/layoutExample/MasterLayoutPage.js
+++ src/sandbox/layoutExample/MasterLayoutPage.js
@@ -5,20 +5,28 @@ master layout
 
 - main container
 	- header
+
 		- side menu bar
+
 		- content container
 			- breadcrumbs
 			- content
 				- header
 				- other stuff
+
 	- footer
 */
 
+// Main Container
 class MasterLayoutPage extends Component {
   render() {
     return (
       <div>
-      	<h1>THE MASTER LAYOUT PAGE</h1>
+				<h1>THE MASTER LAYOUT PAGE</h1>
+				<div>Header Component</div>
+				<div>Side Menu Component</div>
+				<div>Content Container</div>
+				<div>Footer Component</div>
       </div>
     );
   }
