e8982452e340546c5d611f3407031aa0b3acb80b
James Lewis <hijameslewis@gmail.com>
2018-02-09 11:44:49 -0500

    move sandbox routes into main index

--- src/index.js
+++ src/index.js
@@ -4,15 +4,32 @@ import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'react-router-redux'
 import store from './store'
 import history from './history'
+import Route from 'react-router-dom/Route'
+import { MuiThemeProvider, getMuiTheme } from 'material-ui/styles'
 import App from './pages/layout' 
+import CreateTestDataPage from './pages/sandbox/crudExample/new/CreateTestPage'
+import ViewTestDataPage from './pages/sandbox/crudExample/view/ViewTestDataPage'
+import ListTestDataPage from './pages/sandbox/crudExample/list/ListTestDataPage'
+import MasterLayoutPage from './pages/sandbox/layoutExample/MasterLayoutPage'
 
 const target = document.querySelector('#root')
+
+const muiTheme = getMuiTheme({
+  spacing: 1000,
+  fontFamily: 'Roboto, sans-serif',
+});
 
 render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <div>
-      	<App />
+        <MuiThemeProvider muiTheme={muiTheme}>
+	      	<App />
+	      	<Route exact path="/sandbox/crudExample" component={ListTestDataPage} />
+	        <Route exact path="/sandbox/layoutExample" component={MasterLayoutPage} />
+	        <Route exact path="/sandbox/crudExample/new" component={CreateTestDataPage} />
+	        <Route exact path="/sandbox/crudExample/view/:id" component={ViewTestDataPage} />
+				</MuiThemeProvider>
       </div>
     </ConnectedRouter>
   </Provider>,
--- src/pages/layout/index.js
+++ src/pages/layout/index.js
@@ -2,37 +2,21 @@ import React, { Component } from 'react';
 import Route from 'react-router-dom/Route'
 import Home from '../home'
 import About from '../about'
-import { MuiThemeProvider, getMuiTheme } from 'material-ui/styles'
 import VisibleHeader from '../../styleGuide/header/VisibleHeader'
 import VisibleMenuDrawer from '../../styleGuide/menuDrawer/VisibleMenuDrawer'
-import CreateTestDataPage from '../sandbox/crudExample/new/CreateTestPage'
-import ViewTestDataPage from '../sandbox/crudExample/view/ViewTestDataPage'
-import ListTestDataPage from '../sandbox/crudExample/list/ListTestDataPage'
-import MasterLayoutPage from '../sandbox/layoutExample/MasterLayoutPage'
 
 export default class App extends Component {
 
-  muiTheme = getMuiTheme({
-    spacing: 1000,
-    fontFamily: 'Roboto, sans-serif',
-  });
-
   render() {
     return (
-      <MuiThemeProvider muiTheme={this.muiTheme}>
-        <div>
-         <VisibleHeader />
-          <main>
-            <VisibleMenuDrawer />
-            <Route exact path="/" component={Home} />
-            <Route exact path="/about-us" component={About} />
-            <Route exact path="/sandbox/crudExample" component={ListTestDataPage} />
-            <Route exact path="/sandbox/layoutExample" component={MasterLayoutPage} />
-            <Route exact path="/sandbox/crudExample/new" component={CreateTestDataPage} />
-            <Route exact path="/sandbox/crudExample/view/:id" component={ViewTestDataPage} />
-          </main>
-        </div>
-      </MuiThemeProvider>
+      <div>
+       <VisibleHeader />
+        <main>
+          <VisibleMenuDrawer />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about-us" component={About} />
+        </main>
+      </div>
     );
   }
 }
