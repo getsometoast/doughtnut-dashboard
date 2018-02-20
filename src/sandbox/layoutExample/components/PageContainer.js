076df6a7c2e3e59fbe4e943a7a5a00cabb9bf68b
James Lewis <hijameslewis@gmail.com>
2018-02-19 22:10:56 -0500

    remove redundant map dispatch to props function

--- src/sandbox/layoutExample/components/PageContainer.js
+++ src/sandbox/layoutExample/components/PageContainer.js
@@ -1,15 +1,10 @@
 import { connect } from 'react-redux';
 import PageComponent from './PageComponent'
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-  }
-}
-
 function mapStateToProps(state, ownProps) {
   return {
  		content: state.sandbox.layoutExample.contentPages.find(item => item.id === ownProps.match.params.id),
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(PageComponent);
+export default connect(mapStateToProps, null)(PageComponent);
