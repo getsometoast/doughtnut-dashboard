cd2427f7cf5628c3b7e1cf62b9b425cbdc42b4e8
James Lewis <jameslewis@jameslewis.prodvpn.etsy.com>
2018-03-06 13:38:26 -0500

    update test screens for finances with dummy data

--- src/finances/blow/dailyExpenses/index.js
+++ src/finances/blow/dailyExpenses/index.js
@@ -1,11 +1,77 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
+import TextField from 'material-ui/TextField';
+import { RaisedButton } from 'material-ui'
 
 class DailyExpenses extends Component {
+
+  constructor(props) {
+    super(props);
+
+    const tableData = [
+      	{name: 'Groceries', frequency: 'Monthly', 'amount': 1000},
+      	{name: 'Rent', frequency: 'Monthly', 'amount': 5000},
+      	{name: 'Internet', frequency: 'Monthly', 'amount': 100},
+      	{name: 'Gym', frequency: 'Monthly', 'amount': 200},
+      	{name: 'Phone', frequency: 'Monthly', 'amount': 100},
+      	{name: 'Cable', frequency: 'Monthly', 'amount': 100},
+      	{name: 'Netflix', frequency: 'Monthly', 'amount': 10},
+      	{name: 'Spotify', frequency: 'Monthly', 'amount': 10},
+      	{name: 'Pharmacy', frequency: 'Monthly', 'amount': 500},
+      	{name: 'Clothes', frequency: 'Monthly', 'amount': 500}
+      ];
+
+    this.state = {
+      tableData: tableData,
+      filterText: '',
+      filteredData: Object.create(tableData),
+    };
+  }
+
+  handleTextInput = (event) => {
+    const state = this.state;
+    state.filterText = event.target.value.toLowerCase();
+    state.filteredData = this.state.tableData.filter(item => item.name.toLowerCase().startsWith(state.filterText) || item.status.toLowerCase().startsWith(state.filterText) )
+    if(!state.filterText) {
+    	state.filteredData = this.state.tableData;
+    }
+    this.setState(state);
+  }
+
   render() {
     return (
-    <div>
-     Some overview of daily DailyExpenses
-    </div>
+      <div>
+        <h1>Daily Expenses Budgets</h1>
+
+        <TextField onChange={this.handleTextInput} style={{'margin-right': '50px'}}/>
+
+          <Table>
+				    <TableHeader>
+				      <TableRow>
+				        <TableHeaderColumn>Name</TableHeaderColumn>
+				        <TableHeaderColumn>Frequency</TableHeaderColumn>
+				        <TableHeaderColumn>Amount</TableHeaderColumn>
+				      </TableRow>
+				    </TableHeader>
+				    <TableBody>
+	            {this.state.filteredData.map((item, index) => {
+	                return(
+	                  <TableRow>
+				        			<TableRowColumn>{item.name}</TableRowColumn>
+				        			<TableRowColumn>{item.frequency}</TableRowColumn>
+											<TableHeaderColumn>{item.amount}</TableHeaderColumn>
+				      			</TableRow>);
+	              })}
+						</TableBody>
+				  </Table>
+      </div>
     );
   }
 }
--- src/finances/blow/overview/index.js
+++ src/finances/blow/overview/index.js
@@ -1,42 +1,73 @@
 import React, {Component} from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import { TextField } from 'material-ui'
 
 class Overview extends Component {
   render() {
     return (
     <div>
       <Grid fluid style={{paddingTop: '1rem'}}>
-        <h1>Finances: Overview</h1>
+        <h1>BLOW</h1>
         <Row style={{marginBottom: '1rem'}}>
-          <Col mdOffset={4} md={2}>
-            <h3>Daily Expenses:</h3>
+          J: $<TextField value='180,000'/>
+        </Row>
+        <Row style={{marginBottom: '1rem'}}>
+          H: $<TextField value='100,000'/>
+        </Row>
+        <Row style={{marginBottom: '1rem'}}>
+          <Col md={3}>
+            <h3>Daily:</h3>
           </Col>
-          <Col md={6}>
+          <Col md={3}>
+            %<TextField value='40'/>
+          </Col>
+          <Col md={3}>
             <h3>$5000</h3>
           </Col>
+          <Col md={3}>
+            <h3>Budget: $3000</h3>
+          </Col>          
         </Row>
         <Row style={{marginBottom: '1rem'}}>
-          <Col mdOffset={4} md={2}>
+          <Col md={3}>
             <h3>Freedom:</h3>
           </Col>
-          <Col md={6}>
+          <Col md={3}>
+            %<TextField value='10'/>
+          </Col>
+          <Col md={3}>
             <h3>$1000</h3>
           </Col>
+          <Col md={3}>
+            <h3>Current: $990</h3>
+          </Col>
         </Row>
         <Row style={{marginBottom: '1rem'}}>
-          <Col mdOffset={4} md={2}>
+          <Col md={3}>
             <h3>Smile:</h3>
           </Col>
-          <Col md={6}>
-            <h3>$10000</h3>
+          <Col md={3}>
+            %<TextField value='10'/>
+          </Col>
+          <Col md={3}>
+            <h3>$1000</h3>
+          </Col>
+          <Col md={3}>
+            <h3>Budgets: $10000</h3>
           </Col>
         </Row>
         <Row style={{marginBottom: '1rem'}}>
-          <Col mdOffset={4} md={2}>
-            <h3>Fire Extinguisher:</h3>
+          <Col md={3}>
+            <h3>Fire:</h3>
           </Col>
-          <Col md={6}>
-            <h3>$100000</h3>
+          <Col md={3}>
+            %<TextField value='40'/>
+          </Col>
+          <Col md={3}>
+            <h3>$5000</h3>
+          </Col>
+          <Col md={3}>
+            <h3>Goals: $200000</h3>
           </Col>
         </Row>
       </Grid>
